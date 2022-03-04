8c663d703ccd91a4a91b4ab686f5d76483478185
loydp <54460504+loydp@users.noreply.github.com>
2022-03-04 15:21:52 -0800

    Started project, put in placeholder class to start with.

---
+++ src/Parallel_AStar.java
@@ -0,0 +1,14 @@
+public class Parallel_AStar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        // initialize maze
+        // initialize visuals
+        // initialize alg
+        // run a series of tests or something
+
+
+    }
+
+}
