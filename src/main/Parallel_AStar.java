4cee40dc92814fb1143c9836838ccad11e386200
loydp <54460504+loydp@users.noreply.github.com>
2022-03-04 15:46:51 -0800

    Updated project structure

---
+++ src/main/Parallel_AStar.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Parallel_AStar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        // settings = settings
+        // mazes = setUpMaze()
+        // aStar = setUpAStar(mazes, settings)
+        // results = aStar.run()
+        // viz = Visuals
+        // viz(results)
+
+    }
+
+}
