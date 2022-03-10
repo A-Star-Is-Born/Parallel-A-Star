6d269f1032da90b3615554aeaa4cb5197ebf40e5
Ulysses Lin <utemoc@gmail.com>
2022-03-10 11:35:18 -0800

    UL: Print utility

--- src/Display.java
+++ src/Display.java
@@ -1,10 +1,83 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Display {
-    
+    private int dim;
+    private String[][] consoleGrid;
+
+    public Display (int dim) {
+        this.dim = dim;
+        consoleGrid = new String[dim][dim];
+    }
+
+    /**
+     * Prints path to console
+     * 
+     * @param target final Node
+     */
+    public void print(Node target) {
+        System.out.println("\n");
+
+        Node n = target;
+        if(n==null)
+            System.out.println("Something went wrong");
+
+        List<Point> path = new ArrayList<>();
+
+        while(n.parent != null){
+            path.add(n.getCoordinates());
+            n = n.parent;
+        }
+
+        path.add(n.getCoordinates());
+        Collections.reverse(path);
+
+        for(int l = 0; l < path.size(); l++) {
+            Point curr = path.get(l), next;
+            String toUse = "*";
+
+            if (l == 0) {
+                toUse = "S";
+            } else if (l == path.size() - 1) {
+                toUse = "E";
+            } else {
+                next = path.get(l + 1);
+                if (next.x - curr.x == 1) {
+                    toUse = ">";
+                }
+                if (next.x - curr.x == -1) {
+                    toUse = "<";
+                }
+                if (next.y - curr.y == 1) {
+                    toUse = "^";
+                }
+                if (next.y - curr.y == -1) {
+                    toUse = "V";
+                }
+            }
+               
+            consoleGrid[curr.y - 1][curr.x - 1] = toUse;
+        }
+        Collections.reverse(Arrays.asList(consoleGrid));
+
+        for (int y = -1; y < dim + 1; y++) {
+                System.out.print("|");
+            for (int x = 0; x < dim; x++) {
+                if (y == -1 || y == dim) {
+                    System.out.print("-");
+                } else {
+                    System.out.print(consoleGrid[y][x] == null ? " " : consoleGrid[y][x]);
+                }
+            }
+            System.out.print("|\n");
+        }
+
+        System.out.println("\n");
+    }
+
     public String getPath(Node target){
         StringBuilder str = new StringBuilder();
 
@@ -22,8 +95,9 @@ public class Display {
         path.add(n.getCoordinates());
         Collections.reverse(path);
 
-        for(Point step : path)
-            str.append(step.toString() + " ");
+        for(Point step : path) {
+            str.append("-->[" + step.x + "][" + step.y + "]");
+        }
 
         return str.toString();
     }
--- src/Parallel_AStar.java
+++ src/Parallel_AStar.java
@@ -1,13 +1,14 @@
 public class Parallel_AStar {
+    private static final int DIM = 4;
 
     public static void main(String[] args) {
 
         //Initializing. Not super relevant now
         AStar aStar = new AStar();
-        Display display = new Display();
+        Display display = new Display(DIM);
 
         // though maze does take a dimension of 10
-        Maze maze = new Maze(4);
+        Maze maze = new Maze(DIM);
         // System.out.println("maze init/ gen finished");
 
         // Take AStar, and ask it to run the maze.
@@ -17,8 +18,9 @@ public class Parallel_AStar {
         // Do we even want a display path? Not sure.
         // might make sense to just turn it into testing only.
         String printable = display.getPath(res);
-
         System.out.println(printable);
+        display.print(res);
+
 
 
     }
