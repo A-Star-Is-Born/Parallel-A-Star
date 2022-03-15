4c653a4bc1333bf9716ef25c7fc92fe8fd8ee264
loydp <54460504+loydp@users.noreply.github.com>
2022-03-14 22:25:50 -0700

    Removed irrelevant warning in PQR

--- src/ParallelPriorityQueue.java
+++ src/ParallelPriorityQueue.java
@@ -64,9 +64,10 @@ public class ParallelPriorityQueue {
         }
 
         // make sure no threads are blocking while waiting for an empty queue
+        System.out.println("Before interrupt");
         for (Thread t : pqArray)
             t.interrupt();
-
+        System.out.println("After interrupt");
         // make sure all threads are done
         try {
             for (Thread t : pqArray)
