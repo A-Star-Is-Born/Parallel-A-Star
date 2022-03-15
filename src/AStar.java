749e1d9679964cd43e3073b6b43ae3a9fe2ca85c
loydp <54460504+loydp@users.noreply.github.com>
2022-03-14 21:37:58 -0700

    MazeForTesting made

--- src/AStar.java
+++ src/AStar.java
@@ -19,13 +19,13 @@ import java.util.PriorityQueue;
 public class AStar {
     private Node start;     // the node we're starting at
     private Node target;    // the node we're searching for
-    private Test_Maze maze;      // the maze we poll for graph information
+    private Maze maze;      // the maze we poll for graph information
 
     /**
      * Constructor sets up the nature of the problem.
      * @param maze The object that will be polled for graph information.
      */
-    public AStar(Test_Maze maze) {
+    public AStar(Maze maze) {
         this.maze = maze;
         this.start = maze.getStart();
         this.target = maze.getTarget();
