e74c11c0b313c0a22100239722b85d2275ccc4be
loydp <54460504+loydp@users.noreply.github.com>
2022-03-14 20:32:31 -0700

    Further commenting and reorganizing in PQ

--- src/ParallelPriorityQueue.java
+++ src/ParallelPriorityQueue.java
@@ -1,25 +1,48 @@
-import java.util.concurrent.*;
+/**
+ * @author Anh Tran, Peter Loyd, Ulysses Lin
+ * @date 3/14/2022
+ * @see "Seattle University, CPSC5600, Winter 2022"
+ * @class ParallelPriorityQueue.java
+ *
+ * A parallel shared-memory A* search
+ * Uses multiple threads to find a solution using the A* algorithm in parallel.
+ */
 
+import java.awt.*;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * ParallelPriorityQueue sets up and runs a parallel A* algorithm,
+ * using shared memory in the form of SynchronousQueues
+ */
 public class ParallelPriorityQueue {
 
-    private final int N_THREADS;
-    private static SynchronousQueue<Node> targetQueue;
+    private final int N_THREADS;                        // Number of threads desired
+    private static SynchronousQueue<Node> targetQueue;  // How the class communicates with threads
 
+    /**
+     * Constructor for the ParallelPriorityQueue
+     * @param numThreads The number of threads this algorithm will use.
+     */
     public ParallelPriorityQueue(int numThreads) {
         this.N_THREADS = numThreads;
         targetQueue = new SynchronousQueue<>();
     }
 
     /**
-     * Given a maze, applies a heuristic in order to navigate through that maze
-     * and report statistics on that navigation.
+     * Initiates the A* algorithm, sends a number of threads to work in parallel
+     * then waits for one of them to return a solution, and initiates the end-state.
+     * Finally, returns the the solution for processing elsewhere.
+     * @param maze The maze that A* is operating in.
+     * @return Returns the target node.
      */
     public Node run(Maze maze) {
         // initialize data structures
         PriorityBlockingQueue<Node> frontier = new PriorityBlockingQueue<>();
         PriorityBlockingQueue<Node> visited = new PriorityBlockingQueue<>();
 
-        // initialize the first node and put it in the queue
+        // Find the start and the target, initialize the start
         Node start = maze.getStart();
         Node target = maze.getTarget();
         start.g = 0.0;
@@ -29,7 +52,6 @@ public class ParallelPriorityQueue {
 
         // store threads for later
         Thread[] pqArray = new Thread[N_THREADS];
-
         for (int i = 0; i < N_THREADS; i++) {
             pqArray[i] = new Thread(new PriorityQueueRunnable(frontier, visited, targetQueue, maze));
             pqArray[i].start();
@@ -41,9 +63,11 @@ public class ParallelPriorityQueue {
             System.out.println("Error in PPQ trying to take from the queue.");
         }
 
+        // make sure no threads are blocking while waiting for an empty queue
         for (Thread t : pqArray)
             t.interrupt();
 
+        // make sure all threads are done
         try {
             for (Thread t : pqArray)
                 t.join();
@@ -51,11 +75,14 @@ public class ParallelPriorityQueue {
             System.out.println("Exception joining threads: " + e);
         }
 
-
-
         return result;
     }
 
+    /**
+     * Driver class for testing.
+     * @param args Not used.
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws InterruptedException {
         int DIM = 20;
         int numThreads = 4;
@@ -72,7 +99,7 @@ public class ParallelPriorityQueue {
 
         System.out.println("Shortest path length: " + display.getShortestPathLength(res));
 
-        // display.animateShortestPath(res);
+        display.animateShortestPath(res, Color.magenta, .3);
     }
 
 }
--- src/PriorityQueueRunnable.java
+++ src/PriorityQueueRunnable.java
@@ -1,14 +1,38 @@
+/**
+ * @author Anh Tran, Peter Loyd, Ulysses Lin
+ * @date 3/14/2022
+ * @see "Seattle University, CPSC5600, Winter 2022"
+ * @class PriorityQueueRunnable.java
+ *
+ * A parallel shared-memory A* search algorithm
+ * This class is run in parallel to effectively create multithreaded A* path-search.
+ */
 import java.util.ArrayList;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.SynchronousQueue;
 
+/**
+ * PriorityQueueRunnable performs A* search in parallel.
+ * It uses shared priority queues, and calls on a maze to locate
+ * neighboring vertices.
+ */
 public class PriorityQueueRunnable implements Runnable {
-    private final PriorityBlockingQueue<Node> frontier;
-    private final PriorityBlockingQueue<Node> visited;
-    private final SynchronousQueue<Node> targetQueue;
-    private final Maze maze;
-    private final Node target;
+    private final PriorityBlockingQueue<Node> frontier;     // Observed nodes, to be processed
+    private final PriorityBlockingQueue<Node> visited;      // Visited nodes
+    private final SynchronousQueue<Node> targetQueue;       // Signals the work is done
+    private final Maze maze;        // contains information about what nodes are connected
+    private final Node target;      // contains the location of the target node to be reached
+    private static double assumedFinalWeight;
 
+    /**
+     * Initialization works just like it would for sequential A* search,
+     * we create two queues that track where we're going and where we've been,
+     * and we establish what our goal is.
+     * @param frontier Tracks what nodes we're considering going to next.
+     * @param visited Tracks what nodes we've visited in the past.
+     * @param targetQueue The vehicle by which a node signals the search is over.
+     * @param maze Allows the algorithm to find neighboring nodes, and understand wall locations.
+     */
     public PriorityQueueRunnable (PriorityBlockingQueue<Node> frontier,
                                   PriorityBlockingQueue<Node> visited,
                                   SynchronousQueue<Node> targetQueue,
@@ -18,23 +42,40 @@ public class PriorityQueueRunnable implements Runnable {
         this.maze = maze;
         this.targetQueue = targetQueue;
         this.target = maze.getTarget();
+        this.assumedFinalWeight = Double.MAX_VALUE;
     }
 
+    /**
+     * A* search works by putting nodes onto a priority queue, which is then
+     * accessed to find the best next node to check. When it accesses a node
+     * it will find its neighbors using the maze object, and put its neighbors
+     * onto the queue as well.
+     * Upon finding the target it initiates an end-state in which nodes cannot work
+     * on anything that's less optimal than the current path to the target.
+     */
     public void run() {
         Node current;
 
         while(true) {
 
+            //A failsafe, in case interrupt is called but the node is still working on low-priority nodes.
             if (Thread.interrupted())
                 return;
 
             try {
+                // Grab the most promising location, and if it's the target initiate end-state.
                 current = frontier.take();
                 if (current == target) {
                     targetQueue.put(current);
+                    setAssumedFinalWeight(current.f);
                     return;
                 }
+                // No need to work on something we know is worse than optimal
+                if (current.f > assumedFinalWeight) {
+                    continue;
+                }
             } catch (InterruptedException e) {
+                System.out.println("Error when acquiring node" + e);
                 return;
             }
 
@@ -46,7 +87,6 @@ public class PriorityQueueRunnable implements Runnable {
 
                     double fValue = totalWeight + maze.getHeuristic(neighbor.x, neighbor.y, target.getCoordinates());
                     processGValue(current, neighbor, totalWeight, fValue);
-
                     frontier.add(neighbor);
                 } else {
                     if (totalWeight < neighbor.g) {
@@ -58,7 +98,7 @@ public class PriorityQueueRunnable implements Runnable {
                                 visited.remove(neighbor);
                                 frontier.add(neighbor);
                             } catch (UnsupportedOperationException e) {
-                                System.out.println("PQRunnable: tried to remove a neighbor that wasn't there");
+                                // This is an acceptable state of affairs, it does not affect functionality.
                             }
                         }
                     }
@@ -70,11 +110,26 @@ public class PriorityQueueRunnable implements Runnable {
         }
     }
 
+    /**
+     * Synchronized access to node information is necessary, as otherwise
+     * parallel threads could improperly overwrite or improperly sort a node.
+     * @param current       // The node A* is currently on
+     * @param neighbor      // The node being updated with weights
+     * @param totalWight    // the totalWeight up to this point
+     * @param fValue        // the final value of this node, as measured by the priority queue
+     */
     public synchronized void processGValue(Node current, Node neighbor, double totalWight, double fValue) {
         if (totalWight < neighbor.g) {
             neighbor.g = totalWight;
             neighbor.f = fValue;
             neighbor.parent = current;
+        }
+    }
+
+
+    private synchronized void setAssumedFinalWeight(double weight) {
+        if (assumedFinalWeight > weight) {
+            assumedFinalWeight = weight;
         }
     }
 
