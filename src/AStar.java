a954c8e6fab5c656bf30cb405a7f399c68ddb697
loydp <54460504+loydp@users.noreply.github.com>
2022-03-14 21:28:35 -0700

    AStar commented

--- src/AStar.java
+++ src/AStar.java
@@ -1,12 +1,31 @@
+/**
+ * @author Anh Tran, Peter Loyd, Ulysses Lin
+ * @date 3/14/2022
+ * @see "Seattle University, CPSC5600, Winter 2022"
+ * @class AStar.java
+ *
+ * A sequential shared-memory A* search
+ * Uses multiple threads to find a solution using the A* algorithm in parallel.
+ */
+
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
+/**
+ * A standard A* graph search algorithm.
+ * Takes in a maze, a start location and a target,
+ * returns the target, which can be used to derive the path.
+ */
 public class AStar {
-    private Node start;
-    private Node target;
-    private Maze maze;
+    private Node start;     // the node we're starting at
+    private Node target;    // the node we're searching for
+    private Test_Maze maze;      // the maze we poll for graph information
 
-    public AStar(Maze maze) {
+    /**
+     * Constructor sets up the nature of the problem.
+     * @param maze The object that will be polled for graph information.
+     */
+    public AStar(Test_Maze maze) {
         this.maze = maze;
         this.start = maze.getStart();
         this.target = maze.getTarget();
@@ -14,19 +33,19 @@ public class AStar {
 
     /**
      * Given a maze, applies a heuristic in order to navigate through that maze
-     * and report statistics on that navigation.
+     * adds nodes to a priority queue based on their closeness (as defined by
+     * the heuristic) to the target. Then retrieves nodes based on how close
+     * they are to the target + the cost it took to get there.
      */
     public Node run(){
         PriorityQueue<Node> visited = new PriorityQueue<>();
         PriorityQueue<Node> frontier = new PriorityQueue<>();
 
+        // The weight of the start is 0, since start is 0 distance from itself.
         start.g = 0.0;
         start.h = maze.getHeuristic(start.x, start.y, target.getCoordinates());
         start.f = start.g + start.h;
         frontier.add(start);
-
-        // System.out.println("in Astar, just before alg starts");
-
 
         /*
         This will run until the frontier, nodes that are visible but
@@ -44,20 +63,13 @@ public class AStar {
             // call on the maze to find the neighbors of that node
             ArrayList<Node> neighbors = maze.getNeighbors(current);
 
-            // look at neighbors, and
+            // look at neighbors, and process them.
             for (Node node : neighbors) {
                 Node m = node;
                 double totalWeight = current.g + m.weight;
 
-                /*
-                System.out.println("in Astar, looking at the node");
-                System.out.println("size: " + frontier.size());
-                System.out.println("x = " + frontier.peek().x);
-                System.out.println("y = " + frontier.peek().y);
-                */
-
                 // if we have not been to the node, and it
-                // is not already in the list to explore...
+                // is not already in the list to explore, we go here
                 if(!frontier.contains(m) && !visited.contains(m)) {
                     // it knows this node is how to get to it
                     m.parent = current;
@@ -66,6 +78,7 @@ public class AStar {
                     m.f = m.g + maze.getHeuristic(m.x, m.y, target.getCoordinates());
                     frontier.add(m);
                 } else {
+                    // else if we are re-calculating a place we've already seen
                     if (totalWeight < m.g) {
                         m.parent = current;
                         m.g = totalWeight;
@@ -77,13 +90,11 @@ public class AStar {
                     }
                 }
             }
-
             frontier.remove(current);
             visited.add(current);
         }
+        // return null if there is no solution.
         return null;
-
     }
-
 
 }
